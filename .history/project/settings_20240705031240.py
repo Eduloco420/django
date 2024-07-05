e857ea325ac1727048ffc3b215502872a6a31de2
edu <ed.bravod@duocuc.cl>
2024-07-05 03:17:29 -0400

    Render.com Config

--- .history/project/settings_20240705031240.py
+++ .history/project/settings_20240705031240.py
@@ -130,14 +130,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
-
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
